3c8ca0a6967c429274704f482439026d1d8dd729
nooose <hd15807@gmail.com>
2023-03-19 15:06:08 +0900

    feat: Cold, Hot Publisher 예제 코드 작성

---
+++ src/main/java/ch03/HotPublisherExample.java
@@ -0,0 +1,19 @@
+package ch03;
+
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.processors.PublishProcessor;
+
+public class HotPublisherExample {
+    public static void main(String[] args) {
+        PublishProcessor<Integer> processor = PublishProcessor.create();
+        processor.subscribe(data -> System.out.println("구독자1: " + data));
+        processor.onNext(1);
+        processor.onNext(3);
+
+        processor.subscribe(data -> System.out.println("구독자2: " + data));
+        processor.onNext(5);
+        processor.onNext(7);
+
+        processor.onComplete();
+    }
+}
